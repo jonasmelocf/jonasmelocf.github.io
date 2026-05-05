d688855c3b7e9bd9d719ebe2b3a661419d7bd4ec
DJJJonas <djjjonas02@gmail.com>
2026-05-04 23:42:53 -0300

    feature: add disabled style to button

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -13,10 +13,13 @@ const model = defineModel();
 		v-model="model"
 		:class="merge(`
     w-full h-7 p-2
+    text-neutral-200
     bg-neutral-900
     border border-neutral-800 rounded
     inset-shadow-xs inset-shadow-black
     text-base
+    disabled:bg-neutral-800
+    disabled:text-neutral-500
   `, props.class)"
 	>
 </template>
