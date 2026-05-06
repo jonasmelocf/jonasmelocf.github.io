f77ea8a61411483114e430cb2a2c3b61a1b69d17
DJJJonas <djjjonas02@gmail.com>
2026-05-06 00:43:52 -0300

    chore: fix tailwind classes

--- src/features/puzzle/components/TestCaseForm.vue
+++ src/features/puzzle/components/TestCaseForm.vue
@@ -38,33 +38,20 @@ defineExpose({
 
 <template>
 	<div
-		class="group/main relative grid gap-4 bg-linear-to-br from-neutral-800 to-neutral-900 border border-neutral-800 rounded-xl p-6 text-xs text-white"
+		class="group/main relative grid gap-4 bg-(--vp-c-bg) rounded p-6 text-xs"
 	>
 		<button
 			type="button"
 			@click="e => emit('click:close', e)"
-			class="absolute top-2 right-2"
+			class="absolute top-2 right-2 group-hover/main:opacity-70 transition opacity-0 group-hover:opacity-100 hover:opacity-100 rounded-full p-0.5 bg-(--vp-button-alt-bg)"
 		>
-			<X
-				:class="[`
-  group-hover/main:opacity-70
-  transition
-  opacity-0 group-hover:opacity-100
-  hover:opacity-100
-  text-neutral-500
-  size-4 rounded-full
-  p-0.5
-  bg-neutral-800/60
-`]"
-			/>
+			<X class="size-3.5" />
 		</button>
 		<!-- Input field -->
 		<div class="group grid gap-2.5">
-			<Label class="transition group-hover:text-neutral-400"
-				>{{ t("Input") }}</Label
-			>
+			<Label>{{ t("Input") }}</Label>
 			<Input v-model="inputRef" @change="validateInput" />
-			<Label v-if="inputErrorRef" class="text-red-500"
+			<Label v-if="inputErrorRef" class="text-(--vp-c-danger-1)"
 				>{{ inputErrorRef }}</Label
 			>
 		</div>
