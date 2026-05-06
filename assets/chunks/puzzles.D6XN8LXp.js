c41971ca4e7e5bda82e3915fb01366848d321783
DJJJonas <djjjonas02@gmail.com>
2026-05-06 20:48:56 -0300

    Updates

--- assets/chunks/puzzles.D6XN8LXp.js
+++ assets/chunks/puzzles.D6XN8LXp.js
@@ -49,8 +49,7 @@ console.log(n);`,tests:[{input:[1],expects:"*"},{input:[2],expects:`*
 **********`}]},p={id:"hypotenuse",code:`const [x, y] = input();
 console.log(x, y);`,tests:[{input:[3,4],expects:"5"},{input:[5,12],expects:"13"},{input:[8,15],expects:"17"},{input:[7,24],expects:"25"},{input:[20,21],expects:"29"},{input:[9,40],expects:"41"},{input:[12,35],expects:"37"},{input:[11,60],expects:"61"},{input:[16,63],expects:"65"},{input:[28,45],expects:"53"},{input:[33,56],expects:"65"},{input:[48,55],expects:"73"},{input:[13,84],expects:"85"},{input:[36,77],expects:"85"}]},s={id:"fibonacci",code:`const [n] = input();
 console.log(n);`,tests:[{input:[1],expects:"0"},{input:[2],expects:"1"},{input:[3],expects:"1"},{input:[8],expects:"13"},{input:[35],expects:"5_702_887"},{input:[96],expects:"31_940_434_634_990_099_905"},{input:[98],expects:"83_621_143_489_848_422_977"},{input:[99],expects:"135_301_852_344_706_746_049"},{input:[100],expects:"218_922_995_834_555_169_026"},{input:[116],expects:"483_162_952_612_010_163_284_885"},{input:[119],expects:"2_046_711_111_473_984_623_691_759"},{input:[120],expects:"3_311_648_143_516_982_017_180_081"},{input:[121],expects:"5_358_359_254_990_966_640_871_840"},{input:[122],expects:"8_670_007_398_507_948_658_051_921"}]},c={id:"square",code:`const [c, y, x] = input();
-console.log(c, s);`,tests:[{input:[1,"~","~"],expects:`~
-~`},{input:[2,"-","X"],expects:`--
+console.log(c, s);`,tests:[{input:[1,"~","~"],expects:"~"},{input:[2,"-","X"],expects:`--
 --`},{input:[3,"=","|"],expects:`===
 | |
 ===`},{input:[5,".",","],expects:`.....
