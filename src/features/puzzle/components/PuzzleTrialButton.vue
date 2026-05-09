19dcb67d1ce2c0ee465b8e6461bfd49832582b4b
DJJJonas <djjjonas02@gmail.com>
2026-05-09 19:47:06 -0300

    chore: make model required

--- src/features/puzzle/components/PuzzleTrialButton.vue
+++ src/features/puzzle/components/PuzzleTrialButton.vue
@@ -13,19 +13,24 @@ const props = withDefaults(
 );
 
 const puzzle = defineModel<Puzzle>("puzzle", { required: true });
-const progress = defineModel<PuzzleProgress>("progress");
+const progress = defineModel<PuzzleProgress>("progress", { required: true });
 
 defineExpose({ puzzle, progress });
 </script>
 
 <template>
-	<Button ref="progress-buttons" :disabled="progress?.puzzleState === 'locked'" :title="puzzle.id" :class="['size-10 *:p-0.5 *:mx-auto rounded-full p-0 text-(--vp-c-white)!', {
-		'bg-(--vp-c-success-3)': progress?.puzzleState === 'done',
-		'bg-(--vp-c-yellow-3)': progress?.puzzleState === 'unlocked',
+	<Button
+		ref="progress-buttons"
+		:disabled="progress?.puzzleState === 'locked'"
+		:title="puzzle.id"
+		:class="['size-10 *:p-0.5 *:mx-auto rounded-full p-0 text-(--vp-c-white)!', {
+		'bg-(--vp-c-success-1)': progress?.puzzleState === 'done',
+		'bg-(--vp-c-yellow-1)': progress?.puzzleState === 'unlocked',
 		'shadow-black': active,
-	}]">
-		<Lock v-if="!progress || progress.puzzleState === 'locked'" />
-		<LockOpen v-if="progress?.puzzleState === 'unlocked'" />
-		<Check v-if="progress?.puzzleState === 'done'" />
+	}]"
+	>
+		<Lock v-if="progress.puzzleState === 'locked'" />
+		<LockOpen v-if="progress.puzzleState === 'unlocked'" />
+		<Check v-if="progress.puzzleState === 'done'" />
 	</Button>
 </template>
