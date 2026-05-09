42a9da0ff560255c1463310187f8fba1aba124f8
DJJJonas <djjjonas02@gmail.com>
2026-05-09 19:46:44 -0300

    chore: add more services

--- src/features/puzzle/puzzle-progress.service.ts
+++ src/features/puzzle/puzzle-progress.service.ts
@@ -1,5 +1,34 @@
 import { tryOr } from "@/lib/utils";
-import type { PuzzleProgress, PuzzleProgressMap } from "./puzzle.types";
+import type { Puzzle, PuzzleProgress, PuzzleProgressMap } from "./puzzle.types";
+
+export function syncPuzzleProgress(
+	puzzles: Puzzle[],
+	save = true,
+): PuzzleProgressMap {
+	const map = getPuzzleProgressMap();
+	for (const puzzle of puzzles) {
+		if (map[puzzle.id] === undefined) {
+			map[puzzle.id] = {
+				puzzleId: puzzle.id,
+				puzzleState: "locked",
+				lastCode: undefined,
+			};
+		}
+	}
+
+	const firstPuzzle = puzzles[0];
+	if (firstPuzzle) {
+		const firstProgress = map[firstPuzzle.id];
+		if (firstProgress.puzzleState === "locked") {
+			firstProgress.puzzleState = "unlocked";
+		}
+	}
+
+	if (save) {
+		savePuzzleProgressMap(map);
+	}
+	return map;
+}
 
 export function getPuzzleProgressMap(): PuzzleProgressMap {
 	const item = localStorage.getItem("puzzle-progress-map");
@@ -15,9 +44,17 @@ export function getPuzzleProgress(puzzleId: string) {
 	return savePuzzleProgress({ puzzleId, puzzleState: "locked" });
 }
 
+export function savePuzzleProgressMap(map: PuzzleProgressMap) {
+	localStorage.setItem("puzzle-progress-map", JSON.stringify(map));
+}
+
 export function savePuzzleProgress(progress: PuzzleProgress): PuzzleProgress {
 	const map = getPuzzleProgressMap();
 	map[progress.puzzleId] = progress;
 	localStorage.setItem("puzzle-progress-map", JSON.stringify(map));
 	return progress;
+}
+
+export function deletePuzzleProgressMap() {
+	localStorage.removeItem("puzzle-progress-map");
 }
