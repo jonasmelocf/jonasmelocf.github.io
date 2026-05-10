803134ac35011a64b3e70f2bea8ba75eb8976957
DJJJonas <djjjonas02@gmail.com>
2026-05-10 03:00:13 -0300

    feature: make PuzzleDisplay save progress in local storage

--- src/features/puzzle/components/PuzzleDisplay.vue
+++ src/features/puzzle/components/PuzzleDisplay.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import type { Puzzle } from "../puzzle.types";
-import { getPuzzleProgress } from "../puzzle-progress.service";
+import {
+	getPuzzleProgress,
+	savePuzzleProgress,
+} from "../puzzle-progress.service";
 import PuzzleExamples from "./PuzzleExamples.vue";
 import PuzzleIDE from "./PuzzleIDE.vue";
 
 const props = defineProps<{
 	puzzle: Puzzle;
 }>();
-const code = getPuzzleProgress(props.puzzle.id).lastCode ?? props.puzzle.code;
+const code = ref(
+	getPuzzleProgress(props.puzzle.id).lastCode ?? props.puzzle.code,
+);
+
+function saveProgress() {
+	savePuzzleProgress({
+		puzzleId: props.puzzle.id,
+		puzzleState: "done",
+		lastCode: code.value,
+	});
+}
 </script>
 <template>
 	<div class="grid gap-1">
 		<PuzzleExamples :puzzle />
-		<PuzzleIDE :puzzle :code />
+		<PuzzleIDE :puzzle :code @success="saveProgress" />
 	</div>
 </template>
