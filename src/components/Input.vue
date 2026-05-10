d9c21a31819f6007f1ec238e2642a5d5ee733794
DJJJonas <djjjonas02@gmail.com>
2026-05-09 23:44:32 -0300

    feature: restyle input component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import type { InputHTMLAttributes } from "vue";
 import { merge } from "@/lib/utils";
 
 const props = defineProps<{
 	class?: string;
+	type?: InputHTMLAttributes["type"];
 }>();
 
 const model = defineModel();
@@ -11,14 +13,7 @@ const model = defineModel();
 <template>
 	<input
 		v-model="model"
-		:class="merge(`
-    outline-none
-    h-7 p-2
-    border rounded
-    border-(--vp-input-border-color)
-    bg-(--vp-input-bg-color)
-    disabled:bg-neutral-800
-    disabled:text-neutral-500
-  `, props.class)"
+		:type="props.type"
+		:class="merge(`h-8 w-full min-w-0 rounded-lg border border-(--vp-input-border-color) bg-transparent px-2.5 py-1 text-base transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-(--vp-text-3) placeholder:text-(--vp-text-2) focus-visible:border-(--vp-c-border) focus-visible:ring-3 focus-visible:ring-(--vp-c-border)/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-(--vp-input-bg-color)/50 disabled:opacity-50 aria-invalid:border-(--vp-c-red-3) aria-invalid:ring-3 aria-invalid:ring-(--vp-c-red-3)/20 md:text-sm dark:bg-(--vp-input-bg-color)/30 dark:disabled:bg-(--vp-input-bg-color)/80 dark:aria-invalid:border-(--vp-c-red-3)/50 dark:aria-invalid:ring-(--vp-c-red-3)/40 `, props.class)"
 	>
 </template>
--- src/components/UIDebugger.vue
+++ src/components/UIDebugger.vue
@@ -1,26 +1,36 @@
 <script setup lang="ts">
 import { Lock } from "@lucide/vue";
 import Button from "./Button.vue";
+import Field from "./Field.vue";
+import Input from "./Input.vue";
 
+// biome-ignore format: input types
 const buttonVariants = [
-	"primary",
-	"secondary",
-	"sponsor",
-	"gray",
-	"indigo",
-	"purple",
-	"green",
-	"yellow",
-	"red",
+	"primary", "secondary", "sponsor",
+	"gray", "indigo", "purple",
+	"green", "yellow", "red",
 ] as const;
 const buttonSizes = ["lg", "default", "sm", "xs"] as const;
+
+// biome-ignore format: input types
 const buttonIconSizes = [
-	"icon-circle-xl",
-	"icon-lg",
-	"icon",
-	"icon-sm",
-	"icon-xs",
+	"icon-circle-xl", "icon-lg",
+	"icon", "icon-sm", "icon-xs",
 ] as const;
+
+// biome-ignore format: input types
+const inputTypes = [
+	"button", "number",
+	"checkbox", "color",
+	"date", "datetime-local",
+	"email", "file",
+	"hidden", "month",
+	"password", "radio",
+	"range", "reset",
+	"search", "tel",
+	"url", "week",
+] as const;
+
 const capitalize = (text: string) => text[0].toUpperCase() + text.slice(1);
 </script>
 <template>
@@ -35,16 +45,37 @@ const capitalize = (text: string) => text[0].toUpperCase() + text.slice(1);
 			</Button>
 		</div>
 
-		<div v-for="variant in buttonVariants" class="flex gap-1 flex-wrap items-end">
+		<div
+			v-for="variant in buttonVariants"
+			class="flex gap-1 flex-wrap items-end"
+		>
 			<Button v-for="size in buttonSizes" :variant :size>
 				{{ variant }} {{ size }}
 			</Button>
 		</div>
 
-		<div v-for="variant in buttonVariants" class="flex gap-1 flex-wrap items-end">
-			<Button v-for="size in buttonIconSizes" :variant :size :title="`${variant} ${size}`">
+		<div
+			v-for="variant in buttonVariants"
+			class="flex gap-1 flex-wrap items-end"
+		>
+			<Button
+				v-for="size in buttonIconSizes"
+				:variant
+				:size
+				:title="`${variant} ${size}`"
+			>
 				<Lock />
 			</Button>
 		</div>
+
+		<h2>Inputs</h2>
+
+		<Field v-for="type in inputTypes" :label="type" inline>
+			<Input
+				:type
+				:value="type === 'file' ? undefined : type"
+				class="min-w-16"
+			/>
+		</Field>
 	</div>
 </template>
